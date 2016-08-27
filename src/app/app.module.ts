2db3eaff82ed380eac66dba2aac9c8518e46ac14
Evan Wallace <evanwallace@Evans-MacBook-Pro.local>
2016-08-27 20:40:35 +1000

    feat(2) add material design 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MdCoreModule } from '@angular2-material/core';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdCardModule } from '@angular2-material/card';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdIconModule } from '@angular2-material/icon';
+
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ 
+    BrowserModule,
+
+    // Material Design
+    MdCoreModule,
+    MdProgressBarModule,
+    MdRadioModule,
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule
+    
+    ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
